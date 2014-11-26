1cde89828a31f5dd9ee949996ead2c58ac8329a8
Nicholas Pappagalo <npappagalo@gmail.com>
2014-11-25 19:48:56 -0800

    Users are now automatically signed in after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params) #Not the final implementation
+  	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
-  		# Handle a successful save.
   	else
   		render 'new'
   	end
   end
+
+  private
 
   def user_params
   	params.require(:user).permit(:name, :email, :password,
