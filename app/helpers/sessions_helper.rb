0191607a9232d41c454632f0c836e91217029eb0
Nicholas Pappagalo <npappagalo@gmail.com>
2014-12-15 19:24:25 -0800

    Fixed multiple browser log out bugs

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,14 +15,14 @@ module SessionsHelper
 	# Returns the user corresponding to the remember token cookie.
 	def current_user
 		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: user_id)
-		elsif (user_id = cookies.signed[:user_id])
-			user = User.find_by(id: user_id)
-			if user && user.authenticated?(cookies[:remember_token])
-				log_in user
-				@current_user = user
-			end
-		end
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
 	end
 
 	# Returns true if the user is logged in, false otherwise.
@@ -30,8 +30,16 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	# Forgets a persistent session
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	# Logs out the current user.
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
